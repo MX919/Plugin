4c568238e5ad720727f8de90609b9c7527ca13b4
FULLOFMX <55388043@qq.com>
2017-08-14 21:36:14 +0800

    Ajax

--- ajax-get-post.js
+++ ajax-get-post.js
@@ -1,12 +1,12 @@
 
 function ajax(method,url,data,async,success){
-		
+
 		if(window.XMLHttpRequest){
 			xhr=new XMLHttpRequest();
 		}else{
 			xhr=new ActiveXObject('Microsoft.XMLHTTP');
 		}
-	
+		
 		xhr.open(method,url+'?'+data,async);
 		if(method=='post'){
 			xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded;charset=UTF-8");
@@ -14,7 +14,7 @@ function ajax(method,url,data,async,success){
 		}else{
 			xhr.send();
 		}
-		
+
 		xhr.onreadystatechange=function(){
 			if(xhr.readyState==4){
 				if(xhr.status==200){
